42dc85b7ca5302d86f0e5d12cec1b6291e2ab97c
Neha021989 <33144384+Neha021989@users.noreply.github.com>
2018-04-25 09:15:14 +0530

    Update SpringMongoDbDemo1Application.java

--- src/main/java/com/example/demo/SpringMongoDbDemo1Application.java
+++ src/main/java/com/example/demo/SpringMongoDbDemo1Application.java
@@ -2,12 +2,19 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-@EnableEurekaClient
+//@EnableEurekaClient
 @SpringBootApplication
-public class SpringMongoDbDemo1Application {
+public class SpringMongoDbDemo1Application  extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringMongoDbDemo1Application.class, args);
 	}
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpringMongoDbDemo1Application.class);
+	}
+
 }
