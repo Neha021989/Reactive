8b8793279c4478f310c9c1f5fa66533d8b25e218
Neha021989 <33144384+Neha021989@users.noreply.github.com>
2018-04-25 17:41:44 +0530

    Update CustomerRepository.java

--- src/main/java/com/example/demo/repository/CustomerRepository.java
+++ src/main/java/com/example/demo/repository/CustomerRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
 import org.springframework.data.querydsl.binding.QuerydslBindings;
 import org.springframework.stereotype.Repository;
 import com.example.demo.model.Customer;
-import com.example.demo.model.QCustomer;
+
 @Repository
 public interface CustomerRepository extends MongoRepository<Customer, String>,QuerydslPredicateExecutor<Customer>,QuerydslBinderCustomizer<QCustomer> {
 
